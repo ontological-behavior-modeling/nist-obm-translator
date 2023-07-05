dad76b31daa548774a5a7926f9f7b12d0aa48138
Andrew Shinjo <ashinjo3@elsys.gtri.org>
2023-07-05 17:54:55 -0400

    Created FoodServiceControlFlowExplicitFactTest

---
+++ src/test/java/manualtest/FoodServiceControlFlowExplicitFactTest.java
@@ -0,0 +1,14 @@
+package manualtest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FoodServiceControlFlowExplicitFactTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
