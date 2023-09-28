51b885a9c2aeb8cc07dbadea65d60d093f7de02c
Miyako Wilson <miyako.wilson@gatech.edu>
2023-09-28 15:13:47 -0400

    Modify test file and class based on Jeremy's 9/25/23 update pass up to 1st example in 4.1.4

--- src/test/java/obmtest/OBMXMI2AlloyTest.java
+++ src/test/java/obmtest/OBMXMI2AlloyTest.java
@@ -21,33 +21,34 @@ class OBMXMI2AlloyTest {
   @ParameterizedTest
 
   @CsvSource({
+
       "4.1.4 Transfers and Parameters2 - ParameterBehavior.als,Model::Basic::ParameterBehavior",
       "4.1.5 Multiple Execution Steps - Multiple Object Flow_mw.als, Model::Basic::ObjectFlowBehavior",
+
       "4.2.2 FoodService Object Flow - IFSingleFoodService - OFFoodService_mw.als, Model::Realistic::IFFoodService",
-      "4.1.1 Control Nodes1 - SimpleSequence_modified.als,Model::Basic::SimpleSequence",
-      "4.1.1 Control Nodes2 - Fork_modified.als, Model::Basic::BehaviorFork",
-      "4.1.1 Control Nodes3 - Join_modified.als, Model::Basic::BehaviorJoin",
-      "4.1.1 Control Nodes4 - Decision_modified.als, Model::Basic::BehaviorDecision",
-      "4.1.1 Control Nodes5 - Merge_modified.als, Model::Basic::BehaviorMerge",
-      "4.1.1 Control Nodes6 - CombinedControlNodes_modified.als, Model::Basic::ComplexBehavior",
-      // failing fact is different
-      "4.1.2 LoopsExamples_modified.als, Model::Basic::Loop",
-      "4.1.3 CallingBehaviors_modified.als, Model::Basic::ComposedBehavior",
-      "4.1.4 Transfers and Parameters1 - TransferProduct_modified.als, Model::Basic::ParticipantTransfer",
-      "4.1.5 Multiple Execution Steps - Multiple Control Flow_modified.als, Model::Basic::ControlFlowBehavior",
+      "4.1.1 Control Nodes1 - SimpleSequence_mw.als, Model::Basic::SimpleSequence",
+      "4.1.1 Control Nodes2 - Fork.als, Model::Basic::Fork",
+      "4.1.1 Control Nodes3 - Join.als, Model::Basic::Join",
+      "4.1.1 Control Nodes4 - Decision_mw.als, Model::Basic::Decision",
+      "4.1.1 Control Nodes5 - Merge_mw.als, Model::Basic::Merge",
+      "4.1.1 Control Nodes6 - CombinedControlNodes_mw.als, Model::Basic::AllControl",
+      "4.1.2 LoopsExamples.als, Model::Basic::Loop",
+      "4.1.3 CallingBehaviors.als, Model::Basic::ComposedBehavior",
+      "4.1.4 Transfers and Parameters1 - TransferProduct_mw.als, Model::Basic::TransferProduct",
+      "4.1.5 Multiple Execution Steps - Multiple Control Flow.als, Model::Basic::ControlFlowBehavior",
       // 4.1.6
       // fails because p2 multiplicity
-      "4.1.6 UnsatisfiableAsymmetry_mw.als, Model::Basic::UnsatisfiableAsymmetry",
-      "4.1.6 UnsatisfiableTransitivity_mw.als, Model::Basic::UnsatisfiableTransitivity",
-      "4.1.6 UnsatisfiableMultiplicity_mw.als, Model::Basic::UnsatisfiableMultiplicity",
-      "4.1.6 UnsatisfiableComposition1_mw.als, Model::Basic::UnsatisfiableComposition1",
-      "4.1.6 UnsatisfiableComposition2_mw.als, Model::Basic::UnsatisfiableComposition2",
-      "4.2.1 FoodService Control Flow - FoodService_mw.als, Model::Realistic::FoodService",
-      "4.2.1 FoodService Control Flow - SingleFoodService_mw.als, Model::Realistic::SingleFoodService",
-      "4.2.1 FoodService Control Flow - BuffetService_mw.als, Model::Realistic::BuffetService",
-      "4.2.1 FoodService Control Flow - ChurchSupperService_mw.als, Model::Realistic::ChurchSupper",
-      "4.2.1 FoodService Control Flow - FastFoodService_mw.als, Model::Realistic::FastFoodService",
-      "4.2.1 FoodService Control Flow - UsatisfiableFoodService_mw.als, Model::Realistic::UnsatisfiableService",
+      "4.1.6 UnsatisfiableAsymmetry.als, Model::Basic::UnsatisfiableAsymmetry",
+      "4.1.6 UnsatisfiableTransitivity.als, Model::Basic::UnsatisfiableTransitivity",
+      "4.1.6 UnsatisfiableMultiplicity.als, Model::Basic::UnsatisfiableMultiplicity",
+      "4.1.6 UnsatisfiableComposition1.als, Model::Basic::UnsatisfiableComposition1",
+      "4.1.6 UnsatisfiableComposition2.als, Model::Basic::UnsatisfiableComposition2",
+      "4.2.1 FoodService Control Flow - FoodService.als, Model::Realistic::FoodService",
+      "4.2.1 FoodService Control Flow - SingleFoodService.als, Model::Realistic::SingleFoodService",
+      "4.2.1 FoodService Control Flow - BuffetService.als, Model::Realistic::BuffetService",
+      "4.2.1 FoodService Control Flow - ChurchSupperService.als, Model::Realistic::ChurchSupper",
+      "4.2.1 FoodService Control Flow - FastFoodService.als, Model::Realistic::FastFoodService",
+      "4.2.1 FoodService Control Flow - UsatisfiableFoodService.als, Model::Realistic::UnsatisfiableService",
   //
   // TransferBefore
   // // wip waiting Jeremy's update obm file
@@ -80,15 +81,18 @@ class OBMXMI2AlloyTest {
 
     // ========== Create Alloy model from OBM XMI file & write as a file ==========
 
-    OBMXMI2Alloy test = new OBMXMI2Alloy();
-    File xmiFile = new File("src/test/resources/OBMModel_R.xmi");
+    String working_dir = "src/test/resources/obm-alloy-code_2023-09-25/obm";
+    OBMXMI2Alloy test = new OBMXMI2Alloy(working_dir);
+
+    File testing_dir = new File(working_dir);
+    File xmiFile = new File(testing_dir, "OBMModel.xmi");
 
     // File xmiFile = new File(
     // "C:/Users/mw107/Documents/Projects/NIST
     // OBM/GIT/NIST-OBM-Translator.git/develop_mw/target/classes/OBMModel_MW.xmi");
     System.out.println("XMIFile: " + xmiFile.exists() + "? " + xmiFile.getAbsolutePath());
 
-    File apiFile = new File("src/test/resources/" + manualFileName + "_Generated-"
+    File apiFile = new File(testing_dir, manualFileName + "_Generated-"
         + className.replaceAll("::", "_") /* alloyModule.getModuleName() */ + ".als");
     test.createAlloyFile(xmiFile, className, apiFile);
 
@@ -99,7 +103,7 @@ class OBMXMI2AlloyTest {
     // API
     CompModule apiModule = MyAlloyLibrary.importAlloyModule(apiFile);
     // TEST
-    File testFile = new File("src/test/resources/" + manualFileName);
+    File testFile = new File(testing_dir, manualFileName);
     System.out.println("testFile: " + testFile.exists() + "? " + testFile.getAbsolutePath());
     CompModule testModule = MyAlloyLibrary.importAlloyModule(testFile);
 
