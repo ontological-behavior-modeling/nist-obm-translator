205cfd66a740e43c19312dd1dc86936c35f36815
Miyako Wilson <miyako.wilson@gatech.edu>
2024-10-14 15:42:06 -0400

    Modify 4.1.2 Loop::Loop to 4.1.2 Loops::Loop to match with OBMModel.xmi

--- src/test/java/obmtest/OBMXMI2AlloyTest.java
+++ src/test/java/obmtest/OBMXMI2AlloyTest.java
@@ -4,13 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
-
-import edu.gatech.gtri.obm.alloy.translator.AlloyUtils;
-import edu.gatech.gtri.obm.alloy.translator.OBMXMI2Alloy;
-import edu.mit.csail.sdg.ast.Expr;
-import edu.mit.csail.sdg.ast.Sig;
-import edu.mit.csail.sdg.parser.CompModule;
-import edu.umd.omgutil.UMLModelErrorException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
@@ -19,13 +12,19 @@ import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import edu.gatech.gtri.obm.alloy.translator.AlloyUtils;
+import edu.gatech.gtri.obm.alloy.translator.OBMXMI2Alloy;
+import edu.mit.csail.sdg.ast.Expr;
+import edu.mit.csail.sdg.ast.Sig;
+import edu.mit.csail.sdg.parser.CompModule;
+import edu.umd.omgutil.UMLModelErrorException;
 
 /**
  * JUnit Test for translation.
  *
- * <p>The Xmi model is loaded in memory first. Then each test creates a translator generate file.
- * After that the generated file and the expected file are loaded in memory as two alloy objects.
- * Then module(model) name, signatures/fields, and facts are compared.
+ * <p>
+ * The Xmi model is loaded in memory first. Then each test creates a translator generate file. After that the generated file and the expected file are loaded in memory as two alloy objects. Then
+ * module(model) name, signatures/fields, and facts are compared.
  *
  * @author Miyako Wilson, AE(ASDL) - Georgia Tech
  * @author Andrew H Shinjo, Graduate Student - Georgia Tech
@@ -50,79 +49,74 @@ public class OBMXMI2AlloyTest {
   // expected to pass the test, otherwise false) separated by commas.
   @ParameterizedTest
   @CsvSource({
-    "4.1.1 Time Orderings - AllControl.als, Model::4.1 Basic Examples::4.1.1 Time"
-        + " Orderings::AllControl, true",
-    "4.1.1 Time Orderings - Decision.als, Model::4.1 Basic Examples::4.1.1 Time"
-        + " Orderings::Decision, true",
-    "4.1.1 Time Orderings - Fork.als, Model::4.1 Basic Examples::4.1.1 Time Orderings::Fork, true",
-    "4.1.1 Time Orderings - Join.als, Model::4.1 Basic Examples::4.1.1 Time Orderings::Join, true",
-    "4.1.1 Time Orderings - Merge.als, Model::4.1 Basic Examples::4.1.1 Time Orderings::Merge,"
-        + " true",
-    "4.1.1 Time Orderings - SimpleSequence.als, Model::4.1 Basic Examples::4.1.1 Time"
-        + " Orderings::SimpleSequence, true",
-    "4.1.2 Loops - Loop.als, Model::4.1 Basic Examples::4.1.2 Loop::Loop ,true",
-    "4.1.3 Behaviors with Steps - Composed.als, Model::4.1 Basic Examples::4.1.3 Behaviors with"
-        + " Steps::Composed, true",
+      "4.1.1 Time Orderings - AllControl.als, Model::4.1 Basic Examples::4.1.1 Time"
+          + " Orderings::AllControl, true",
+      "4.1.1 Time Orderings - Decision.als, Model::4.1 Basic Examples::4.1.1 Time"
+          + " Orderings::Decision, true",
+      "4.1.1 Time Orderings - Fork.als, Model::4.1 Basic Examples::4.1.1 Time Orderings::Fork, true",
+      "4.1.1 Time Orderings - Join.als, Model::4.1 Basic Examples::4.1.1 Time Orderings::Join, true",
+      "4.1.1 Time Orderings - Merge.als, Model::4.1 Basic Examples::4.1.1 Time Orderings::Merge,"
+          + " true",
+      "4.1.1 Time Orderings - SimpleSequence.als, Model::4.1 Basic Examples::4.1.1 Time"
+          + " Orderings::SimpleSequence, true",
+      "4.1.2 Loops - Loop.als, Model::4.1 Basic Examples::4.1.2 Loops::Loop ,true",
+      "4.1.3 Behaviors with Steps - Composed.als, Model::4.1 Basic Examples::4.1.3 Behaviors with"
+          + " Steps::Composed, true",
 
-    // alloy file in name, qualifiedName of behavior model(class), boolean true if expected to pass,
-    // false if expected to fail
-    "4.1.4 Transfers and Parameters - ParameterBehavior_mw.als, Model::4.1 Basic Examples::4.1.4"
-        + " Transfers and Parameters::ParameterBehavior, true",
-    "4.1.4 Transfers and Parameters - TransferProduct.als, Model::4.1 Basic Examples::4.1.4"
-        + " Transfers and Parameters::TransferProduct, true",
-    "4.1.5 Steps with Multiple Executions - MultipleControlFlow.als, Model::4.1 Basic"
-        + " Examples::4.1.5 Steps with Multiple Executions::MultipleControlFlow, true",
-    "4.1.5 Steps with Multiple Executions - MultipleObjectFlow_mw.als, Model::4.1 Basic"
-        + " Examples::4.1.5 Steps with Multiple Executions::MultipleObjectFlow, true",
-    // expected to fail
-    "4.1.5 Steps with Multiple Executions - MultipleControlFlow - Fail.als, Model::4.1 Basic"
-        + " Examples::4.1.5 Steps with Multiple Executions::MultipleControlFlow, false",
-    "4.1.6 Unsatisfiable - UnsatisfiableAsymmetry.als, Model::4.1 Basic Examples::4.1.6"
-        + " Unsatisfiable::UnsatisfiableAsymmetry, true",
-    "4.1.6 Unsatisfiable - UnsatisfiableComposition1.als, Model::4.1 Basic Examples::4.1.6"
-        + " Unsatisfiable::UnsatisfiableComposition1, true",
-    "4.1.6 Unsatisfiable - UnsatisfiableComposition2.als, Model::4.1 Basic Examples::4.1.6"
-        + " Unsatisfiable::UnsatisfiableComposition2, true",
-    "4.1.6 Unsatisfiable - UnsatisfiableMultiplicity.als, Model::4.1 Basic Examples::4.1.6"
-        + " Unsatisfiable::UnsatisfiableMultiplicity, true",
-    "4.1.6 Unsatisfiable - UnsatisfiableTransitivity.als, Model::4.1 Basic Examples::4.1.6"
-        + " Unsatisfiable::UnsatisfiableTransitivity, true",
-    "4.2.1 Food Service Control Flow - BuffetService.als, Model::4.2 Advanced Examples::4.2.1 Food"
-        + " Service Control Flow::BuffetService, true",
-    "4.2.1 Food Service Control Flow - FastFoodService.als, Model::4.2 Advanced Examples::4.2.1"
-        + " Food Service Control Flow::FastFoodService, true",
-    "4.2.1 Food Service Control Flow - ChurchSupper.als, Model::4.2 Advanced Examples::4.2.1 Food"
-        + " Service Control Flow::ChurchSupper, true",
-    "4.2.1 Food Service Control Flow - RestaurantService.als, Model::4.2 Advanced Examples::4.2.1"
-        + " Food Service Control Flow::RestaurantService, true",
-    "4.2.1 Food Service Control Flow - SingleFoodService.als, Model::4.2 Advanced Examples::4.2.1"
-        + " Food Service Control Flow::SingleFoodService, true",
-    "4.2.1 Food Service Control Flow - UnsatisfiableFoodService.als, Model::4.2 Advanced"
-        + " Examples::4.2.1 Food Service Control Flow::UnsatisfiableFoodService, true",
-    "4.2.2 Food Service Object Flow - OFControlLoopFoodService_mw.als, Model::4.2 Advanced"
-        + " Examples::4.2.2 Food Service Object Flow::OFControlLoopFoodService, true",
-    "4.2.2 Food Service Object Flow - OFSingleFoodService.als, Model::4.2 Advanced Examples::4.2.2"
-        + " Food Service Object Flow::OFSingleFoodService, true",
-    "4.2.2 Food Service Object Flow - OFParallelFoodService.als, Model::4.2 Advanced"
-        + " Examples::4.2.2 Food Service Object Flow::OFParallelFoodService, true",
+      // alloy file in name, qualifiedName of behavior model(class), boolean true if expected to pass,
+      // false if expected to fail
+      "4.1.4 Transfers and Parameters - ParameterBehavior_mw.als, Model::4.1 Basic Examples::4.1.4"
+          + " Transfers and Parameters::ParameterBehavior, true",
+      "4.1.4 Transfers and Parameters - TransferProduct.als, Model::4.1 Basic Examples::4.1.4"
+          + " Transfers and Parameters::TransferProduct, true",
+      "4.1.5 Steps with Multiple Executions - MultipleControlFlow.als, Model::4.1 Basic"
+          + " Examples::4.1.5 Steps with Multiple Executions::MultipleControlFlow, true",
+      "4.1.5 Steps with Multiple Executions - MultipleObjectFlow_mw.als, Model::4.1 Basic"
+          + " Examples::4.1.5 Steps with Multiple Executions::MultipleObjectFlow, true",
+      // expected to fail
+      "4.1.5 Steps with Multiple Executions - MultipleControlFlow - Fail.als, Model::4.1 Basic"
+          + " Examples::4.1.5 Steps with Multiple Executions::MultipleControlFlow, false",
+      "4.1.6 Unsatisfiable - UnsatisfiableAsymmetry.als, Model::4.1 Basic Examples::4.1.6"
+          + " Unsatisfiable::UnsatisfiableAsymmetry, true",
+      "4.1.6 Unsatisfiable - UnsatisfiableComposition1.als, Model::4.1 Basic Examples::4.1.6"
+          + " Unsatisfiable::UnsatisfiableComposition1, true",
+      "4.1.6 Unsatisfiable - UnsatisfiableComposition2.als, Model::4.1 Basic Examples::4.1.6"
+          + " Unsatisfiable::UnsatisfiableComposition2, true",
+      "4.1.6 Unsatisfiable - UnsatisfiableMultiplicity.als, Model::4.1 Basic Examples::4.1.6"
+          + " Unsatisfiable::UnsatisfiableMultiplicity, true",
+      "4.1.6 Unsatisfiable - UnsatisfiableTransitivity.als, Model::4.1 Basic Examples::4.1.6"
+          + " Unsatisfiable::UnsatisfiableTransitivity, true",
+      "4.2.1 Food Service Control Flow - BuffetService.als, Model::4.2 Advanced Examples::4.2.1 Food"
+          + " Service Control Flow::BuffetService, true",
+      "4.2.1 Food Service Control Flow - FastFoodService.als, Model::4.2 Advanced Examples::4.2.1"
+          + " Food Service Control Flow::FastFoodService, true",
+      "4.2.1 Food Service Control Flow - ChurchSupper.als, Model::4.2 Advanced Examples::4.2.1 Food"
+          + " Service Control Flow::ChurchSupper, true",
+      "4.2.1 Food Service Control Flow - RestaurantService.als, Model::4.2 Advanced Examples::4.2.1"
+          + " Food Service Control Flow::RestaurantService, true",
+      "4.2.1 Food Service Control Flow - SingleFoodService.als, Model::4.2 Advanced Examples::4.2.1"
+          + " Food Service Control Flow::SingleFoodService, true",
+      "4.2.1 Food Service Control Flow - UnsatisfiableFoodService.als, Model::4.2 Advanced"
+          + " Examples::4.2.1 Food Service Control Flow::UnsatisfiableFoodService, true",
+      "4.2.2 Food Service Object Flow - OFControlLoopFoodService_mw.als, Model::4.2 Advanced"
+          + " Examples::4.2.2 Food Service Object Flow::OFControlLoopFoodService, true",
+      "4.2.2 Food Service Object Flow - OFSingleFoodService.als, Model::4.2 Advanced Examples::4.2.2"
+          + " Food Service Object Flow::OFSingleFoodService, true",
+      "4.2.2 Food Service Object Flow - OFParallelFoodService.als, Model::4.2 Advanced"
+          + " Examples::4.2.2 Food Service Object Flow::OFParallelFoodService, true",
   })
   /**
    * Compare manually created/expected alloy file and translator generated file.
    *
-   * <p>Initialize the translator by loading the xmiFile check if output_and_testfiles_directory
-   * exists. Then, the translator is reused for all the tests. First, create an alloy file from a
-   * class named sysMLClassQualifiedName from Obm xmi file using Alloy API. Next the created alloy
-   * file is imported using Alloy API again to find AllReachableFacts and
-   * AllReachableUserDefinedSigs. Also, the manually created alloy file (manualFileName) is imported
-   * using Alloy API to find its AllReachableFacts and AllReachableUserDefinedSigs. Finally, the
-   * Sigs(signatures) and Expressions(Reachable facts) of manually created/expected and generated by
-   * translator are compared.
+   * <p>
+   * Initialize the translator by loading the xmiFile check if output_and_testfiles_directory exists. Then, the translator is reused for all the tests. First, create an alloy file from a class named
+   * sysMLClassQualifiedName from Obm xmi file using Alloy API. Next the created alloy file is imported using Alloy API again to find AllReachableFacts and AllReachableUserDefinedSigs. Also, the
+   * manually created alloy file (manualFileName) is imported using Alloy API to find its AllReachableFacts and AllReachableUserDefinedSigs. Finally, the Sigs(signatures) and Expressions(Reachable
+   * facts) of manually created/expected and generated by translator are compared.
    *
    * @param _expectedAlloyFileName(String) - absolute path for expected file name
-   * @param _classQualifiedName(String) - class's qualifedName (i.e., Model::4.1 Basic
-   *     Examples::4.1.1 Time Orderings::Fork)
-   * @param expctedResult(boolean) - true if expected to pass the test, false if expected to fail
-   *     the test
+   * @param _classQualifiedName(String) - class's qualifedName (i.e., Model::4.1 Basic Examples::4.1.1 Time Orderings::Fork)
+   * @param expctedResult(boolean) - true if expected to pass the test, false if expected to fail the test
    */
   public void compare(
       String _expectedAlloyFileName, String _classQualifiedName, boolean _expectedResult) {
@@ -173,7 +167,8 @@ public class OBMXMI2AlloyTest {
     // Compare
     if (_expectedResult)
       assertTrue(ec.compareTwoExpressionsFacts(api_reachableFacts, test_reachableFacts));
-    else assertFalse(ec.compareTwoExpressionsFacts(api_reachableFacts, test_reachableFacts));
+    else
+      assertFalse(ec.compareTwoExpressionsFacts(api_reachableFacts, test_reachableFacts));
 
     ///////////////////////// Comparing Signature ////////////////////
     // API
@@ -201,8 +196,7 @@ public class OBMXMI2AlloyTest {
   }
 
   /**
-   * initialize the translator by loading xmi file and specify which directory translator result
-   * files will be written.
+   * initialize the translator by loading xmi file and specify which directory translator result files will be written.
    */
   private static boolean initializeOBMXMI2Alloy() {
 
